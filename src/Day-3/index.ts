5b9c35d63c3c0b71321104992c19c79c15c397aa
Jurgensvds <jurgvds@gmail.com>
2023-12-06 11:33:23 +1000

    Update index.ts

--- src/Day-3/index.ts
+++ src/Day-3/index.ts
@@ -1,3 +1,6 @@
+Day 3 - Advent
+
+
 import { readTextFile } from "../file-reader";
 import { Coordinate, NumberCoordinate } from "./models/day-three-models";
 
@@ -42,7 +45,7 @@ function partOne(mappedData: string[][]): void {
         }
     }
 
-    console.log(total)
+    console.log('Part one:', total);
 }
 
 function findNumberEndIndex(row: string[], numberStartCoordinate: Coordinate): Coordinate {
@@ -100,7 +103,37 @@ function getNumberFromList(mappedData: string[][], coordinate: NumberCoordinate)
 // Part Two - START
 
 function partTwo(mappedData: string[][]): void {
-    // Start Here
+    const coordinates = mapToCoordinateArray(mappedData);
+    let gearMap: number[][][] = mappedData.map(row => row.map(() => []));;
+    for(let coord of coordinates) {
+        addNumberToGearMap(mappedData, coord, gearMap)
+    }
+
+    gearMap = gearMap.map(row => row.filter((cell) => cell.length === 2)).filter((row) => row.length);
+    const total = gearMap.reduce((a,b) => a + b.reduce((x, y) => x + (y[0] * y[1]), 0), 0)
+    console.log("Part two:", total);
 }
+
+function addNumberToGearMap(mappedData: string[][], coordinates: NumberCoordinate, gearMap: number[][][]): void {
+    let hasAdjacentSymbols = false;
+
+    const startColumn = coordinates.start.column === 0 ? 0 : coordinates.start.column - 1;
+    const endColumn = coordinates.end.column === mappedData.length - 1 ? coordinates.end.column : coordinates.end.column + 1;
+    const startRow = coordinates.start.row === 0 ? 0 : coordinates.start.row - 1;
+    const endRow = coordinates.end.row === mappedData.length - 1 ? coordinates.end.row : coordinates.end.row + 1;
+
+    for(let i = startRow; i <= endRow; i++) {
+        for(let j = startColumn; j <= endColumn; j++) {
+            if(i === coordinates.start.row && (j >= coordinates.start.column && j <= coordinates.end.column)) {
+                continue;
+            }
+
+            if(mappedData[i][j] === '*') {
+                // gearMap[i][j].push(coordinates)
+                gearMap[i][j].push(getNumberFromList(mappedData, coordinates))
+            }
+        }
+    }
+} 
 
 // Part Two - END
